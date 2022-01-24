ee3e3258b87371981d8bcbf98d9c760702bb953a
oykue <oykuergun07@gmail.com>
2022-01-24 11:15:55 +0300

    add get card by user id to user dao

--- src/main/java/atmapplication/dataAccess/CardDao.java
+++ src/main/java/atmapplication/dataAccess/CardDao.java
@@ -4,6 +4,9 @@ import atmapplication.entities.concretes.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CardDao extends JpaRepository<Card, Integer> {
+    Optional<Card> getByUserId(int userId);
 }
