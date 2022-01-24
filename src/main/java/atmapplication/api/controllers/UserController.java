d9ba53f86c703623dc32bcaed73f0f5a3267d18d
oykue <oykuergun07@gmail.com>
2022-01-24 11:18:25 +0300

    fixed autowired method to user

--- src/main/java/atmapplication/api/controllers/UserController.java
+++ src/main/java/atmapplication/api/controllers/UserController.java
@@ -1,24 +1,33 @@
 package atmapplication.api.controllers;
 
 import atmapplication.api.dto.LoginDto;
+import atmapplication.business.abstracts.CardService;
 import atmapplication.business.abstracts.UserService;
 import atmapplication.entities.concretes.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-    @Autowired
+
     private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        super();
+        this.userService = userService;
+    }
 
    @PostMapping("/login-user")
     public ResponseEntity<User> loginUser(@RequestBody LoginDto loginDto){
     return ResponseEntity.ok(userService.login(loginDto.getTckNo(),loginDto.getPassword()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable int id){
+        return ResponseEntity.ok(userService.getUser(id));
     }
 
 }
