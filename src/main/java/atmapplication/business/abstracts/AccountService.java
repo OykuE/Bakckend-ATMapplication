8f79aea4b33c5ad3efda7c3246769ee63cdd531e
oykue <oykuergun07@gmail.com>
2022-01-24 15:52:56 +0300

    add withdraw method to update balance after withdraw money on account service

--- src/main/java/atmapplication/business/abstracts/AccountService.java
+++ src/main/java/atmapplication/business/abstracts/AccountService.java
@@ -4,4 +4,5 @@ import atmapplication.entities.concretes.Account;
 
 public interface AccountService {
 Account accounts(int userId, String accountType);
+Integer withdraw(int userId, String accountType, double withdraw);
 }
