1a23943fe3d803db01fe9600e789ed9066cdea94
oykue <oykuergun07@gmail.com>
2022-01-24 15:31:43 +0300

    add pay off debt data transfer object to update debt about pay off

---
+++ src/main/java/atmapplication/api/dto/PayOffDebtDto.java
@@ -0,0 +1,14 @@
+package atmapplication.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PayOffDebtDto {
+    private int userId;
+    private String cardType;
+    private double payoff;
+}
