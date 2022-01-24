9a3ac423fdfe1c9e20937073c066887235e54c5a
oykue <oykuergun07@gmail.com>
2022-01-24 16:07:52 +0300

    add deposit method to update balance after deposit money on account service

--- src/main/java/atmapplication/business/abstracts/AccountService.java
+++ src/main/java/atmapplication/business/abstracts/AccountService.java
@@ -5,4 +5,5 @@ import atmapplication.entities.concretes.Account;
 public interface AccountService {
 Account accounts(int userId, String accountType);
 Integer withdraw(int userId, String accountType, double withdraw);
+Integer deposit(int userId, String accountType, double deposit);
 }
